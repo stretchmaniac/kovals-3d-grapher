a1cc6c649d72eef0917912da7c4f70263d9697d4
Alan Koval <stretchmaniac8@gmail.com>
2017-08-23 11:28:25 -0400

    Fix single quote in wolfram src bug.

--- Build/result/src/grapher.js
+++ Build/result/src/grapher.js
@@ -1213,7 +1213,7 @@ function onGetLinkClicked(){
 function onEmbedClicked(){
 	domain.miniDisplay = true;
 	let url = getLinkUrl();
-	document.getElementById('embed-button-text').textContent = '<iframe src=\'https://alankoval.com/3dgrapher/src/index.html'+url+'\' width=\'800\' height=\'800\'><p>Your browser does not support iframe</p></iframe>';
+	document.getElementById('embed-button-text').textContent = '<iframe src="https://alankoval.com/3dgrapher/src/index.html'+url+'" width=\'800\' height=\'800\'><p>Your browser does not support iframe</p></iframe>';
 	selectText('embed-button-text');
 	document.getElementById('embed-button').onclick = function(){
 		return false;
@@ -1222,7 +1222,7 @@ function onEmbedClicked(){
 
 function onMathematicaEmbedClicked(){
 	domain.miniDisplay = true;
-	let htmlText = '<iframe src=\'https://alankoval.com/3dgrapher/src/index.html'+getLinkUrl()+'\' width=\'800\' height=\'800\'><p>Your browser does not support iframe</p></iframe>';
+	let htmlText = '<iframe src=\\"https://alankoval.com/3dgrapher/src/index.html'+getLinkUrl()+'\\" width=\'800\' height=\'800\'><p>Your browser does not support iframe</p></iframe>';
 	let finalText = 'EmbeddedHTML["'+htmlText+'"]';
 	document.getElementById('embed-mathematica-text').textContent = finalText;
 	selectText('embed-mathematica-text');
